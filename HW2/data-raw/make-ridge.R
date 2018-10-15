f06f0c21200611ac902e136745464853c174e378
Yahui <yahui.zhao@yale.edu>
2018-10-14 21:43:32 -0400

    test

--- HW2/data-raw/make-ridge.R
+++
@@ -1,7 +0,0 @@
-
-setwd("~/Desktop/bis557/HW2/data-raw")
-ridge_train<- read.csv("ridge_train.csv")
-save(ridge_train,file="../data/ridge_train.rda")
-
-ridge_test<-read.csv("ridge_test.csv")
-save(ridge_test, file="../data/ridge_test.rda")
