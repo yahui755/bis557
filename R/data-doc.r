86e5af9560e461adb5503562822865f83f5dda42
Yahui <yahui.zhao@yale.edu>
2018-10-14 21:30:39 -0400

    testtwo

--- R/data-doc.r
+++ R/data-doc.r
@@ -8,3 +8,18 @@
 #' on linear regressions. This data set gives us a basis for exploration.
 NULL
 
+#' A data set for the second homework
+#'
+#' @name ridge_train
+#' @docType data
+#' @rdname ridge_train
+#' @description the training dataset we use in ridge regression
+"ridge_train"
+
+#' A data set for the second homework
+#'
+#' @name ridge_test
+#' @docType data
+#' @rdname ridge_test
+#' @description the testing dataset we use in ridge regression
+"ridge_test"
